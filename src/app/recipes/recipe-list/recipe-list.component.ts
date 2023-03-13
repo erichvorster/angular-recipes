29affbfa51e0ff78962da70bef163fff6edd2ae9
erichvorster <erichv@infoslips.com>
2023-03-13 11:26:01 +0200

    initial commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+ recipes: Recipe[] = [
+  new Recipe('A text recipe', 'this is a test','https://images.pexels.com/photos/1556688/pexels-photo-1556688.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' )
+ ]
+
+ constructor(){}
+ ngOnInit(){
+   
+ }
+}
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,7 @@
+//note that the below is a shortcut to assigning values provided by typeScript
+
+export class Ingredient {
+    constructor(public name: string, public amount: number) {
+        
+    }
+}
